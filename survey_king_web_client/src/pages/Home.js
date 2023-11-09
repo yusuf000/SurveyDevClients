263bb306fb125c5adc6564ff9e2ceae8ccdb2d16
Abdullah Al Osman Sajid <abdullah.osman@tigerit.com>
2023-11-09 10:26:16 +0600

    Changes related to: 1.Added additional plugins and js

--- survey_king_web_client/src/pages/Home.js
+++ survey_king_web_client/src/pages/Home.js
@@ -4,7 +4,7 @@ import defaultImage from "../img/defaultAvatar.png"
 
 export function Home(){
     return (
-        <div>
+        <div className="hold-transition">
             <meta charSet="utf-8" />
             <meta name="viewport" content="width=device-width, initial-scale=1" />
             <title>AdminLTE 3 | Dashboard 3</title>
