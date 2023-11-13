3efc27a1a688e821231c9565e88b1b4547bcca37
osmansajid <osmansazid13@gmail>
2023-11-14 00:14:27 +0600

    Changes related to: 1.Changed dashboard elements

--- survey_king_web_client/src/layouts/dashboard/index.js
+++ survey_king_web_client/src/layouts/dashboard/index.js
@@ -1,16 +1,16 @@
 /**
-=========================================================
-* Material Dashboard 2 React - v2.2.0
-=========================================================
+ =========================================================
+ * Material Dashboard 2 React - v2.2.0
+ =========================================================
 
-* Product Page: https://www.creative-tim.com/product/material-dashboard-react
-* Copyright 2023 Creative Tim (https://www.creative-tim.com)
+ * Product Page: https://www.creative-tim.com/product/material-dashboard-react
+ * Copyright 2023 Creative Tim (https://www.creative-tim.com)
 
-Coded by www.creative-tim.com
+ Coded by www.creative-tim.com
 
  =========================================================
 
-* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+ * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
  */
 
 // @mui material components
@@ -33,34 +33,50 @@ import DataTable from "../../examples/Tables/DataTable";
 import MDTypography from "../../components/MDTypography";
 import React from "react";
 import Card from "@mui/material/Card";
+import DefaultInfoCard from "../../examples/Cards/InfoCards/DefaultInfoCard";
+import Link from "@mui/material/Link";
 
 function Dashboard() {
 
-  const { sales, tasks } = reportsLineChartData;
+    const {sales, tasks} = reportsLineChartData;
 
-  return (
-    <DashboardLayout>
-      <DashboardNavbar />
-      <MDBox py={3}>
-        <Grid container spacing={3}>
-          <Grid item xs={12} md={6} lg={3}>
-            <MDBox mb={1.5}>
-                <SimpleBlogCard
-                    title="Create Project"
-                    description="Start right away by creating a project and adding questions"
-                    action={{
-                        type: "internal",
-                        route: "/somewhere",
-                        color: "info",
-                        label: "create"
-                    }}
-                />
-            </MDBox>
-          </Grid>
-        </Grid>
-        <MDBox mt={4.5}>
-          <Grid container spacing={3}>
-            {/*<Grid item xs={12} md={6} lg={4}>
+    const handleOnClickProject = () => {
+        console.log('project clicked');
+    }
+
+    return (
+        <DashboardLayout>
+            <DashboardNavbar/>
+            <MDBox py={3}>
+                <Grid container spacing={3}>
+                    <Grid item xs={12} md={6} lg={3}>
+                        <MDBox mb={1.5}>
+                            <SimpleBlogCard
+                                title="Create Project"
+                                description="Start right away by creating a project and adding questions"
+                                action={{
+                                    type: "internal",
+                                    route: "/somewhere",
+                                    color: "info",
+                                    label: "create"
+                                }}
+                            />
+                        </MDBox>
+                    </Grid>
+                    <Grid item xs={12} md={6} lg={3}>
+                        <MDBox mb={1.5}>
+                            <DefaultInfoCard
+                                icon="folder"
+                                title="Total Running Projects"
+                                description="Running projects can still take answers"
+                                value="3"
+                            />
+                        </MDBox>
+                    </Grid>
+                </Grid>
+                <MDBox mt={4.5}>
+                    <Grid container spacing={3}>
+                        {/*<Grid item xs={12} md={6} lg={4}>
               <MDBox mb={3}>
                 <ReportsBarChart
                   color="info"
@@ -71,7 +87,7 @@ function Dashboard() {
                 />
               </MDBox>
             </Grid>*/}
-            {/*<Grid item xs={12} md={6} lg={4}>
+                        {/*<Grid item xs={12} md={6} lg={4}>
               <MDBox mb={3}>
                 <ReportsLineChart
                   color="success"
@@ -97,44 +113,48 @@ function Dashboard() {
                 />
               </MDBox>
             </Grid>*/}
-          </Grid>
-        </MDBox>
-        <MDBox>
-          <Grid container spacing={3}>
-            <Grid item xs={12} md={6} lg={8}>
-                <Card>
-                    <MDTypography variant="h5" fontWeight="medium" color="black" mt={1} my={3} mx={3}>
-                        Your Projects
-                    </MDTypography>
-                    <DataTable
-                        table={{
-                            columns: [
-                                { Header: "project name", accessor: "project_name", width: "35%" },
-                                { Header: "client", accessor: "client"},
-                                { Header: "start date", accessor: "start_date" },
-                                { Header: "end date", accessor: "end_date" },
-                            ],
-                            rows: [
-                                {
-                                    project_name: "X",
-                                    client: "Hanny Baniard",
-                                    start_date: "4/11/2021",
-                                    end_date: "10/11/2021",
-                                }
-                            ]
-                        }}
-                    />
-                </Card>
-            </Grid>
-            {/*<Grid item xs={12} md={6} lg={4}>
+                    </Grid>
+                </MDBox>
+                <MDBox>
+                    <Grid container spacing={3}>
+                        <Grid item xs={12} md={6} lg={8}>
+                            <Card>
+                                <MDTypography variant="h5" fontWeight="medium" color="dark" mt={1} my={3} mx={3}>
+                                    Your Projects
+                                </MDTypography>
+                                <DataTable
+                                    table={{
+                                        columns: [
+                                            {Header: "project name", accessor: "project_name", width: "35%"},
+                                            {Header: "client", accessor: "client"},
+                                            {Header: "start date", accessor: "start_date"},
+                                            {Header: "end date", accessor: "end_date"},
+                                        ],
+                                        rows: [
+                                            {
+                                                project_name: <MDBox>
+                                                    <Link href="#" onClick={handleOnClickProject} color="blue">
+                                                        project-x
+                                                    </Link>
+                                                </MDBox>,
+                                                client: "Hanny Baniard",
+                                                start_date: "4/11/2021",
+                                                end_date: "10/11/2021",
+                                            }
+                                        ]
+                                    }}
+                                />
+                            </Card>
+                        </Grid>
+                        {/*<Grid item xs={12} md={6} lg={4}>
               <OrdersOverview />
             </Grid>*/}
-          </Grid>
-        </MDBox>
-      </MDBox>
-      <Footer/>
-    </DashboardLayout>
-  );
+                    </Grid>
+                </MDBox>
+            </MDBox>
+            <Footer/>
+        </DashboardLayout>
+    );
 }
 
 export default Dashboard;
