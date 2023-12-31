7fa24de981b6dbb65cc15279f400dfaed3f48587
Abdullah Al Osman Sajid <abdullah.osman@tigerit.com>
2023-12-31 14:00:59 +0600

    Changes related to: 1.Landing page related changes

--- survey_king_web_client/src/examples/Navbars/DefaultNavbar/DefaultNavbarMobile.js
+++ survey_king_web_client/src/examples/Navbars/DefaultNavbar/DefaultNavbarMobile.js
@@ -45,8 +45,6 @@ function DefaultNavbarMobile({ open, close }) {
       MenuListProps={{ style: { width: `calc(${width}px - 4rem)` } }}
     >
       <MDBox px={0.5}>
-        <DefaultNavbarLink icon="donut_large" name="dashboard" route="/dashboard" />
-        <DefaultNavbarLink icon="person" name="profile" route="/profile" />
         <DefaultNavbarLink icon="account_circle" name="sign up" route="/authentication/sign-up" />
         <DefaultNavbarLink icon="key" name="sign in" route="/authentication/sign-in" />
       </MDBox>
