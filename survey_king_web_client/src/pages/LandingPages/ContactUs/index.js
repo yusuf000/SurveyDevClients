d33325ea65d2c0f5d89a9aec26cde4fa1bbfb2ea
Abdullah Al Osman Sajid <abdullah.osman@tigerit.com>
2024-01-02 15:39:57 +0600

    Changes related to: 1.Contact us related change

--- survey_king_web_client/src/pages/LandingPages/ContactUs/index.js
+++ survey_king_web_client/src/pages/LandingPages/ContactUs/index.js
@@ -31,17 +31,29 @@ import routes from "routes";
 import footerRoutes from "footer.routes";
 
 // Image
-import bgImage from "assets/images/illustrations/illustration-reset.jpg";
+import bgImage from "assets/images/contact.jpg";
 import Container from "@mui/material/Container";
+import * as emailjs from "emailjs-com";
 
 function ContactUs() {
+    const sendMail = (e)=>{
+        e.preventDefault();
+
+        emailjs.sendForm('survey_devs', 'template_qz8s41j', e.target, 'DXUSxL8zeD4XsUAwL')
+            .then((result) => {
+                window.location.reload()
+            }, (error) => {
+                console.log(error.text);
+            });
+    }
+
     return (
         <>
             <DefaultNavbar
                 routes={routes}
                 sticky
             />
-            <MKBox minHeight="75vh"
+            <MKBox minHeight="100vh"
                    width="100%"
                    sx={{
                        backgroundImage: ({functions: {linearGradient, rgba}, palette: {gradients}}) =>
@@ -55,98 +67,77 @@ function ContactUs() {
                        placeItems: "center",
                    }}>
                 <Container>
-                    <Grid container spacing={3} alignItems="center">
-                        <Grid item xs={12} lg={6}>
-                            <MKBox
-                                display={{xs: "none", lg: "flex"}}
-                                width="calc(100% - 2rem)"
-                                height="calc(80vh - 2rem)"
-                                borderRadius="lg"
-                                ml={2}
-                                mt={2}
-                                sx={{backgroundImage: `url(${bgImage})`}}
-                            />
-                        </Grid>
-                        <Grid
-                            item
-                            xs={12}
-                            sm={10}
-                            md={7}
-                            lg={6}
-                            xl={4}
-                            ml={{xs: "auto", lg: 6}}
-                            mr={{xs: "auto", lg: 6}}
+                    <MKBox
+                        bgColor="white"
+                        borderRadius="xl"
+                        shadow="lg"
+                        display="flex"
+                        flexDirection="column"
+                        justifyContent="center"
+                        mt={{xs: 20, sm: 18, md: 20}}
+                        mb={{xs: 20, sm: 18, md: 20}}
+                        mx={3}
+                    >
+                        <MKBox
+                            variant="gradient"
+                            bgColor="info"
+                            coloredShadow="info"
+                            borderRadius="lg"
+                            p={2}
+                            mx={2}
+                            mt={-3}
                         >
-                            <MKBox
-                                bgColor="white"
-                                borderRadius="xl"
-                                shadow="lg"
-                                display="flex"
-                                flexDirection="column"
-                                justifyContent="center"
-                                mt={{xs: 20, sm: 18, md: 20}}
-                                mb={{xs: 20, sm: 18, md: 20}}
-                                mx={3}
-                            >
-                                <MKBox
-                                    variant="gradient"
-                                    bgColor="info"
-                                    coloredShadow="info"
-                                    borderRadius="lg"
-                                    p={2}
-                                    mx={2}
-                                    mt={-3}
-                                >
-                                    <MKTypography variant="h3" color="white">
-                                        Contact us
-                                    </MKTypography>
-                                </MKBox>
-                                <MKBox p={3}>
-                                    <MKTypography variant="body2" color="text" mb={3}>
-                                        For further questions, including partnership opportunities, please email
-                                        osmansazid13@gmail.com / yusufst000@gmail.com or contact using our contact form.
-                                    </MKTypography>
-                                    <MKBox width="100%" component="form" method="post" autoComplete="off">
-                                        <Grid container spacing={3}>
-                                            <Grid item xs={12} md={6}>
-                                                <MKInput
-                                                    variant="standard"
-                                                    label="Full Name"
-                                                    InputLabelProps={{shrink: true}}
-                                                    fullWidth
-                                                />
-                                            </Grid>
-                                            <Grid item xs={12} md={6}>
-                                                <MKInput
-                                                    type="email"
-                                                    variant="standard"
-                                                    label="Email"
-                                                    InputLabelProps={{shrink: true}}
-                                                    fullWidth
-                                                />
-                                            </Grid>
-                                            <Grid item xs={12}>
-                                                <MKInput
-                                                    variant="standard"
-                                                    label="What can we help you?"
-                                                    placeholder="Describe your problem in at least 250 characters"
-                                                    InputLabelProps={{shrink: true}}
-                                                    multiline
-                                                    fullWidth
-                                                    rows={6}
-                                                />
-                                            </Grid>
-                                        </Grid>
-                                        <Grid container item justifyContent="center" xs={12} mt={5} mb={2}>
-                                            <MKButton type="submit" variant="gradient" color="info">
-                                                Send Message
-                                            </MKButton>
-                                        </Grid>
-                                    </MKBox>
-                                </MKBox>
+                            <MKTypography variant="h3" color="white">
+                                Contact us
+                            </MKTypography>
+                        </MKBox>
+                        <MKBox p={3}>
+                            <MKTypography variant="body2" color="text" mb={3}>
+                                For further questions, including partnership opportunities, please email
+                                osmansazid13@gmail.com / yusufst000@gmail.com or contact using our contact form.
+                            </MKTypography>
+                            <MKBox width="100%" component="form" method="post" autoComplete="off" onSubmit={sendMail}>
+                                <Grid container spacing={3}>
+                                    <Grid item xs={12} md={6}>
+                                        <MKInput
+                                            variant="standard"
+                                            label="Full Name"
+                                            InputLabelProps={{shrink: true}}
+                                            name="full_name"
+                                            fullWidth
+                                        />
+                                    </Grid>
+                                    <Grid item xs={12} md={6}>
+                                        <MKInput
+                                            type="email"
+                                            variant="standard"
+                                            label="Email"
+                                            InputLabelProps={{shrink: true}}
+                                            name="email"
+                                            fullWidth
+                                        />
+                                    </Grid>
+                                    <Grid item xs={12}>
+                                        <MKInput
+                                            variant="standard"
+                                            label="What can we help you?"
+                                            placeholder="Describe your problem in at least 250 characters"
+                                            InputLabelProps={{shrink: true}}
+                                            multiline
+                                            fullWidth
+                                            rows={6}
+                                            name="message"
+                                        />
+                                    </Grid>
+                                </Grid>
+                                <Grid container item justifyContent="center" xs={12} mt={5} mb={2}>
+                                    <MKButton type="submit" variant="gradient" color="info">
+                                        Send Message
+                                    </MKButton>
+                                </Grid>
                             </MKBox>
-                        </Grid>
-                    </Grid>
+                        </MKBox>
+                    </MKBox>
                 </Container>
             </MKBox>
         </>
