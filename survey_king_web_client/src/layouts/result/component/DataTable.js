aff542ce729b6b24913d40acf5933d52ce629159
Abdullah Al Osman Sajid <abdullah.osman@tigerit.com>
2024-01-08 15:38:48 +0600

    Changes related to: 1.Showing descriptive questions answers

---
+++ survey_king_web_client/src/layouts/result/component/DataTable.js
@@ -0,0 +1,273 @@
+/**
+ =========================================================
+ * Material Dashboard 2 React - v2.2.0
+ =========================================================
+
+ * Product Page: https://www.creative-tim.com/product/material-dashboard-react
+ * Copyright 2023 Creative Tim (https://www.creative-tim.com)
+
+ Coded by www.creative-tim.com
+
+ =========================================================
+
+ * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+ */
+
+import React, {useMemo, useEffect, useState} from "react";
+
+// prop-types is a library for typechecking of props
+import PropTypes from "prop-types";
+
+// react-table components
+import {useTable, usePagination, useGlobalFilter, useAsyncDebounce, useSortBy} from "react-table";
+
+// @mui material components
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableContainer from "@mui/material/TableContainer";
+import TableRow from "@mui/material/TableRow";
+import Icon from "@mui/material/Icon";
+import Autocomplete from "@mui/material/Autocomplete";
+
+// Material Dashboard 2 React components
+import MDBox from "components/MDBox";
+import MDTypography from "components/MDTypography";
+import MDInput from "components/MDInput";
+import MDPagination from "components/MDPagination";
+
+// Material Dashboard 2 React example components
+import DataTableHeadCell from "examples/Tables/DataTable/DataTableHeadCell";
+import DataTableBodyCell from "examples/Tables/DataTable/DataTableBodyCell";
+import Divider from "@mui/material/Divider";
+import Grid from "@mui/material/Grid";
+import MDButton from "../../../components/MDButton";
+
+function DataTable({
+                       entriesPerPage,
+                       canSearch,
+                       table,
+                       isSorted,
+                       noEndBorder,
+                       index,
+                       total,
+                       onForward,
+                       onBackward
+                   }) {
+    const defaultValue = entriesPerPage.defaultValue ? entriesPerPage.defaultValue : 5;
+    const entries = entriesPerPage.entries
+        ? entriesPerPage.entries.map((el) => el.toString())
+        : ["5", "10", "15", "20", "25"];
+    const columns = useMemo(() => table.columns, [table]);
+    const data = useMemo(() => table.rows, [table]);
+
+    const tableInstance = useTable(
+        {columns, data, initialState: {pageIndex: 0}},
+        useGlobalFilter,
+        useSortBy,
+        usePagination
+    );
+
+    const {
+        getTableProps,
+        getTableBodyProps,
+        headerGroups,
+        prepareRow,
+        rows,
+        page,
+        pageOptions,
+        canPreviousPage,
+        canNextPage,
+        gotoPage,
+        nextPage,
+        previousPage,
+        setPageSize,
+        setGlobalFilter,
+        state: {pageIndex, pageSize, globalFilter},
+    } = tableInstance;
+
+    // Set the default value for the entries per page when component mounts
+    useEffect(() => setPageSize(defaultValue || 5), [defaultValue]);
+
+    // Set the entries per page value based on the select value
+    const setEntriesPerPage = (value) => setPageSize(value);
+
+    // Render the paginations
+    const renderPagination = pageOptions.map((option) => (
+        <MDPagination
+            item
+            key={option}
+            onClick={() => gotoPage(Number(option))}
+            active={pageIndex === option}
+        >
+            {option + 1}
+        </MDPagination>
+    ));
+
+    // Handler for the input to set the pagination index
+    const handleInputPagination = ({target: {value}}) =>
+        value > pageOptions.length || value < 0 ? gotoPage(0) : gotoPage(Number(value));
+
+    // Customized page options starting from 1
+    const customizedPageOptions = pageOptions.map((option) => option + 1);
+
+    // Setting value for the pagination input
+    const handleInputPaginationValue = ({target: value}) => gotoPage(Number(value.value - 1));
+
+    // Search input value state
+    const [search, setSearch] = useState(globalFilter);
+
+    // Search input state handle
+    const onSearchChange = useAsyncDebounce((value) => {
+        setGlobalFilter(value || undefined);
+    }, 100);
+
+    // A function that sets the sorted value for the table
+    const setSortedValue = (column) => {
+        let sortedValue;
+
+        if (isSorted && column.isSorted) {
+            sortedValue = column.isSortedDesc ? "desc" : "asce";
+        } else if (isSorted) {
+            sortedValue = "none";
+        } else {
+            sortedValue = false;
+        }
+
+        return sortedValue;
+    };
+
+    // Setting the entries starting point
+    const entriesStart = pageIndex === 0 ? pageIndex + 1 : pageIndex * pageSize + 1;
+
+    // Setting the entries ending point
+    let entriesEnd;
+
+    if (pageIndex === 0) {
+        entriesEnd = pageSize;
+    } else if (pageIndex === pageOptions.length - 1) {
+        entriesEnd = rows.length;
+    } else {
+        entriesEnd = pageSize * (pageIndex + 1);
+    }
+
+    return (
+        <TableContainer sx={{boxShadow: "none"}}>
+            {entriesPerPage || canSearch ? (
+                <MDBox display="flex" justifyContent="space-between" alignItems="center" p={3}>
+
+                    {canSearch && (
+                        <MDBox width="12rem" ml="auto">
+                            <MDInput
+                                placeholder="Search..."
+                                value={search}
+                                size="small"
+                                fullWidth
+                                onChange={({currentTarget}) => {
+                                    setSearch(search);
+                                    onSearchChange(currentTarget.value);
+                                }}
+                            />
+                        </MDBox>
+                    )}
+                </MDBox>
+            ) : null}
+            <Table {...getTableProps()}>
+                <MDBox component="thead">
+                    {headerGroups.map((headerGroup, key) => (
+                        <TableRow key={key} {...headerGroup.getHeaderGroupProps()}>
+                            {headerGroup.headers.map((column, idx) => (
+                                <DataTableHeadCell
+                                    key={idx}
+                                    {...column.getHeaderProps(isSorted && column.getSortByToggleProps())}
+                                    width={column.width ? column.width : "auto"}
+                                    align={column.align ? column.align : "left"}
+                                    sorted={setSortedValue(column)}
+                                    value={column.render("Header")}
+                                >
+                                    {(column.render("Header").includes("__")) ? "" : column.render("Header")}
+                                </DataTableHeadCell>
+                            ))}
+                        </TableRow>
+                    ))}
+                </MDBox>
+                <TableBody {...getTableBodyProps()}>
+                    {page.map((row, key) => {
+                        prepareRow(row);
+                        return (
+                            <TableRow key={key} {...row.getRowProps()}>
+                                {row.cells.map((cell, idx) => (
+                                    <DataTableBodyCell
+                                        key={idx}
+                                        noBorder={noEndBorder && rows.length - 1 === key}
+                                        align={cell.column.align ? cell.column.align : "left"}
+                                        {...cell.getCellProps()}
+                                    >
+                                        {cell.render("Cell")}
+                                    </DataTableBodyCell>
+                                ))}
+                            </TableRow>
+                        );
+                    })}
+                </TableBody>
+            </Table>
+            <MDBox mt={4}>
+                <Grid container spacing={1} justifyContent="center" alignItems="center">
+                    <Grid item>
+                        <MDButton onClick={onBackward}>
+                            <Icon color="info">arrow_back_ios</Icon>
+                        </MDButton>
+                    </Grid>
+                    <Grid item>
+                        <MDTypography variant="button">{index}/{total}</MDTypography>
+                    </Grid>
+                    <Grid item>
+                        <MDButton onClick={onForward}>
+                            <Icon color="info">arrow_forward_ios</Icon>
+                        </MDButton>
+                    </Grid>
+                </Grid>
+            </MDBox>
+        </TableContainer>
+    );
+}
+
+// Setting default values for the props of DataTable
+DataTable.defaultProps = {
+    entriesPerPage: {defaultValue: 5, entries: [5, 10, 15, 20, 25]},
+    canSearch: false,
+    showTotalEntries: true,
+    pagination: {variant: "gradient", color: "info"},
+    isSorted: true,
+    noEndBorder: false,
+};
+
+// Typechecking props for the DataTable
+DataTable.propTypes = {
+    entriesPerPage: PropTypes.oneOfType([
+        PropTypes.shape({
+            defaultValue: PropTypes.number,
+            entries: PropTypes.arrayOf(PropTypes.number),
+        }),
+        PropTypes.bool,
+    ]),
+    canSearch: PropTypes.bool,
+    showTotalEntries: PropTypes.bool,
+    table: PropTypes.objectOf(PropTypes.array).isRequired,
+    pagination: PropTypes.shape({
+        variant: PropTypes.oneOf(["contained", "gradient"]),
+        color: PropTypes.oneOf([
+            "primary",
+            "secondary",
+            "info",
+            "success",
+            "warning",
+            "error",
+            "dark",
+            "light",
+        ]),
+    }),
+    isSorted: PropTypes.bool,
+    noEndBorder: PropTypes.bool,
+};
+
+export default DataTable;
--- survey_king_web_client/src/layouts/result/details/index.js
+++ survey_king_web_client/src/layouts/result/details/index.js
@@ -1,11 +1,24 @@
 import DashboardNavbar from "../../../examples/Navbars/DashboardNavbar";
-import React from "react";
+import React, {useEffect, useState} from "react";
 import DashboardLayout from "../../../examples/LayoutContainers/DashboardLayout";
 import Result from "../index";
 import {useLocation, useNavigate} from "react-router-dom";
+import axios from "axios";
+import MDTypography from "../../../components/MDTypography";
+import Icon from "@mui/material/Icon";
+import Grid from "@mui/material/Grid";
+import Card from "@mui/material/Card";
+import MDBox from "../../../components/MDBox";
+import DataTable from "../component/DataTable";
+
+const url = `http://localhost:8080/`
 
 function ResultDetails() {
+    const [answerData, setAnswerData] = useState([])
+    const [currentPage, setCurrentPage] = useState(0)
+    const [totalPage, setTotalPage] = useState()
     const location = useLocation();
+    const navigate = useNavigate();
 
     if (location.state != null) {
         localStorage.setItem("question", location.state.question)
@@ -13,9 +26,117 @@ function ResultDetails() {
 
     const question = JSON.parse(localStorage.getItem('question'));
 
+    function onBackward(){
+        if(currentPage === 0) return
+        const index = currentPage;
+        setCurrentPage(index - 1);
+        loadResponsesList(index - 1);
+    }
+
+    function onForward(){
+        if(currentPage === totalPage - 1) return
+        const index = currentPage;
+        setCurrentPage(index + 1);
+        loadResponsesList(index + 1);
+    }
+
+    const prepareTabledata = (data) => {
+        let tableData = [];
+        for (let i in data) {
+            let item = data[i];
+            tableData.push({
+                "date": item.date,
+                "userName": item.id.userId,
+                "answer": item.answers[0].description
+            });
+
+        }
+        return tableData;
+    }
+
+    const loadResponsesList = (pageNo) => {
+        const token = localStorage.getItem('token');
+        axios
+            .get(url + "api/v1/analytics/responses", {
+                headers: {
+                    'Authorization': 'Bearer ' + token
+                },
+                params: {
+                    questionId: question.id,
+                    pageNo: pageNo
+                }
+            })
+            .then((response) => {
+                if (response.data.content.length !== 0) {
+                    setTotalPage(response.data.totalPages);
+                    setAnswerData(prepareTabledata(response.data.content));
+                }
+            })
+            .catch((e) => {
+                if (e.response.status === 403) {
+                    localStorage.clear();
+                    navigate('/authentication/sign-in')
+                }
+            })
+    }
+
+    function Descriptive() {
+        return (
+            <MDBox>
+                {
+                    answerData ? <MDBox mt={4}>
+                        <Grid container spacing={3}>
+                            <Grid item xs={12} md={6} lg={12}>
+                                {
+                                    <Card>
+                                        <MDTypography variant="h5" fontWeight="medium" color="dark" mt={1} my={3}
+                                                      mx={3}>
+                                            {question.description}
+                                        </MDTypography>
+                                        <DataTable
+                                            entriesPerPage={
+                                                {
+                                                    defaultValue: 10, entries: [5, 10, 15, 20, 25]
+                                                }
+                                            }
+                                            index={currentPage + 1}
+                                            total={totalPage}
+                                            onForward={onForward}
+                                            onBackward={onBackward}
+                                            table={{
+                                                columns: [
+                                                    {Header: "date", accessor: "date"},
+                                                    {Header: "user", accessor: "userName"},
+                                                    {Header: "answer", accessor: "answer", width: "50%"}
+                                                ],
+                                                rows: answerData
+                                            }}/>
+                                    </Card>
+                                }
+                            </Grid>
+                        </Grid>
+                    </MDBox> : null
+                }
+            </MDBox>
+        )
+    }
+
+    function NonDescriptive() {
+
+    }
+
+    useEffect(() => {
+        if (question.questionType.name === "descriptive") {
+            loadResponsesList(0);
+        }
+    }, []);
+
     return (
         <DashboardLayout>
             <DashboardNavbar/>
+            {
+                question.questionType.name === "descriptive" ? <Descriptive/> : <NonDescriptive/>
+            }
         </DashboardLayout>
     )
 }
