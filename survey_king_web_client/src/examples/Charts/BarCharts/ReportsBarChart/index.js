8f3e9e37b81b9c29db043ed29b72fa61d216dc6c
Abdullah Al Osman Sajid <abdullah.osman@tigerit.com>
2024-01-09 13:15:11 +0600

    Changes related to: 1.Showing multiple choice chart data

--- survey_king_web_client/src/examples/Charts/BarCharts/ReportsBarChart/index.js
+++ survey_king_web_client/src/examples/Charts/BarCharts/ReportsBarChart/index.js
@@ -1,33 +1,33 @@
 /**
-=========================================================
-* Material Dashboard 2  React - v2.2.0
-=========================================================
+ =========================================================
+ * Material Dashboard 2  React - v2.2.0
+ =========================================================
 
-* Product Page: https://www.creative-tim.com/product/material-dashboard-react
-* Copyright 2023 Creative Tim (https://www.creative-tim.com)
+ * Product Page: https://www.creative-tim.com/product/material-dashboard-react
+ * Copyright 2023 Creative Tim (https://www.creative-tim.com)
 
-Coded by www.creative-tim.com
+ Coded by www.creative-tim.com
 
  =========================================================
 
-* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
-*/
+ * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+ */
 
-import React, { useMemo } from "react";
+import React, {useMemo} from "react";
 
 // porp-types is a library for typechecking of props
 import PropTypes from "prop-types";
 
 // react-chartjs-2 components
-import { Bar } from "react-chartjs-2";
+import {Bar} from "react-chartjs-2";
 import {
-  Chart as ChartJS,
-  CategoryScale,
-  LinearScale,
-  BarElement,
-  Title,
-  Tooltip,
-  Legend,
+    Chart as ChartJS,
+    CategoryScale,
+    LinearScale,
+    BarElement,
+    Title,
+    Tooltip,
+    Legend,
 } from "chart.js";
 
 // @mui material components
@@ -46,92 +46,94 @@ import MDButton from "../../../../components/MDButton";
 
 ChartJS.register(CategoryScale, LinearScale, BarElement, Title, Tooltip, Legend);
 
-function ReportsBarChart({ color, title, description, date, chart , count, index, onForward, onBackward}) {
-  const { data, options } = configs(chart.labels || [], chart.datasets || {});
+function ReportsBarChart({color, title, description, date, chart, count, index, onForward, onBackward}) {
+    const {data, options} = configs(chart.labels || [], chart.datasets || {});
 
-  return (
-    <Card sx={{ height: "100%" }}>
-      <MDBox padding="1rem">
-        {useMemo(
-          () => (
-            <MDBox
-              variant="gradient"
-              bgColor={color}
-              borderRadius="lg"
-              coloredShadow={color}
-              py={2}
-              pr={0.5}
-              mt={-5}
-              height="12.5rem"
-            >
-              <Bar data={data} options={options} redraw />
+    return (
+        <Card sx={{height: "100%"}}>
+            <MDBox padding="1rem">
+                {useMemo(
+                    () => (
+                        <MDBox
+                            variant="gradient"
+                            bgColor={color}
+                            borderRadius="lg"
+                            coloredShadow={color}
+                            py={2}
+                            pr={0.5}
+                            mt={-5}
+                            height="12.5rem"
+                        >
+                            <Bar data={data} options={options} redraw/>
+                        </MDBox>
+                    ),
+                    [color, chart]
+                )}
+                <MDBox pt={3} pb={1} px={1}>
+                    <MDTypography variant="h6" textTransform="capitalize">
+                        {title}
+                    </MDTypography>
+                    <MDTypography component="div" variant="button" color="text" fontWeight="light">
+                        {description}
+                    </MDTypography>
+                    <Divider/>
+                    {
+                        date ? <MDBox display="flex" alignItems="center">
+                            <MDTypography variant="button" color="text" lineHeight={1} sx={{mt: 0.15, mr: 0.5}}>
+                                <Icon>schedule</Icon>
+                            </MDTypography>
+                            <MDTypography variant="button" color="text" fontWeight="light">
+                                {date}
+                            </MDTypography>
+                        </MDBox> : null
+                    }
+                    {
+                        index ? <MDBox>
+                            <MDBox display="flex" alignItems="center">
+                                <MDTypography variant="button" color="text" lineHeight={1} sx={{mt: 0.15, mr: 0.5}}>
+                                    <Icon>feed</Icon>
+                                </MDTypography>
+                                <MDTypography variant="button" color="text" fontWeight="light">
+                                    {count}
+                                </MDTypography>
+                            </MDBox>
+                            <Divider/>
+                            <Grid container spacing={1} justifyContent="center" alignItems="center">
+                                <Grid item>
+                                    <MDButton onClick={onBackward}>
+                                        <Icon color="info">arrow_back_ios</Icon>
+                                    </MDButton>
+                                </Grid>
+                                <Grid item>
+                                    <MDTypography>{index}</MDTypography>
+                                </Grid>
+                                <Grid item>
+                                    <MDButton onClick={onForward}>
+                                        <Icon color="info">arrow_forward_ios</Icon>
+                                    </MDButton>
+                                </Grid>
+                            </Grid>
+                        </MDBox> : null
+                    }
+                </MDBox>
             </MDBox>
-          ),
-          [color, chart]
-        )}
-        <MDBox pt={3} pb={1} px={1}>
-          <MDTypography variant="h6" textTransform="capitalize">
-            {title}
-          </MDTypography>
-          <MDTypography component="div" variant="button" color="text" fontWeight="light">
-            {description}
-          </MDTypography>
-          <Divider />
-          <MDBox display="flex" alignItems="center">
-            <MDTypography variant="button" color="text" lineHeight={1} sx={{ mt: 0.15, mr: 0.5 }}>
-              <Icon>schedule</Icon>
-            </MDTypography>
-            <MDTypography variant="button" color="text" fontWeight="light">
-              {date}
-            </MDTypography>
-          </MDBox>
-          {
-            index ? <MDBox>
-              <MDBox display="flex" alignItems="center">
-                <MDTypography variant="button" color="text" lineHeight={1} sx={{ mt: 0.15, mr: 0.5 }}>
-                  <Icon>feed</Icon>
-                </MDTypography>
-                <MDTypography variant="button" color="text" fontWeight="light">
-                  {count}
-                </MDTypography>
-              </MDBox>
-              <Divider/>
-              <Grid container spacing={1} justifyContent="center" alignItems="center">
-                <Grid item>
-                  <MDButton onClick={onBackward}>
-                    <Icon color="info">arrow_back_ios</Icon>
-                  </MDButton>
-                </Grid>
-                <Grid item>
-                  <MDTypography>{index}</MDTypography>
-                </Grid>
-                <Grid item>
-                  <MDButton onClick={onForward}>
-                    <Icon color="info">arrow_forward_ios</Icon>
-                  </MDButton>
-                </Grid>
-              </Grid>
-            </MDBox>: null
-          }
-        </MDBox>
-      </MDBox>
-    </Card>
-  );
+        </Card>
+    );
 }
 
 // Setting default values for the props of ReportsBarChart
 ReportsBarChart.defaultProps = {
-  color: "info",
-  description: "",
+    color: "info",
+    description: "",
 };
 
 // Typechecking props for the ReportsBarChart
 ReportsBarChart.propTypes = {
-  color: PropTypes.oneOf(["primary", "secondary", "info", "success", "warning", "error", "dark"]),
-  title: PropTypes.string.isRequired,
-  description: PropTypes.oneOfType([PropTypes.string, PropTypes.node]),
-  date: PropTypes.string.isRequired,
-  chart: PropTypes.objectOf(PropTypes.oneOfType([PropTypes.array, PropTypes.object])).isRequired,
+    color: PropTypes.oneOf(["primary", "secondary", "info", "success", "warning", "error", "dark"]),
+    title: PropTypes.string.isRequired,
+    description: PropTypes.oneOfType([PropTypes.string, PropTypes.node]),
+    date: PropTypes.string.isRequired,
+    chart: PropTypes.objectOf(PropTypes.oneOfType([PropTypes.array, PropTypes.object])).isRequired,
 };
 
 export default ReportsBarChart;
--- survey_king_web_client/src/layouts/result/details/index.js
+++ survey_king_web_client/src/layouts/result/details/index.js
@@ -10,11 +10,13 @@ import Grid from "@mui/material/Grid";
 import Card from "@mui/material/Card";
 import MDBox from "../../../components/MDBox";
 import DataTable from "../component/DataTable";
+import ReportsBarChart from "../../../examples/Charts/BarCharts/ReportsBarChart";
 
 const url = `http://localhost:8080/`
 
 function ResultDetails() {
-    const [answerData, setAnswerData] = useState([])
+    const [answerData, setAnswerData] = useState(null)
+    const [chartData, setChartData] = useState(null)
     const [currentPage, setCurrentPage] = useState(0)
     const [totalPage, setTotalPage] = useState()
     const location = useLocation();
@@ -26,21 +28,21 @@ function ResultDetails() {
 
     const question = JSON.parse(localStorage.getItem('question'));
 
-    function onBackward(){
-        if(currentPage === 0) return
+    function onBackward() {
+        if (currentPage === 0) return
         const index = currentPage;
         setCurrentPage(index - 1);
         loadResponsesList(index - 1);
     }
 
-    function onForward(){
-        if(currentPage === totalPage - 1) return
+    function onForward() {
+        if (currentPage === totalPage - 1) return
         const index = currentPage;
         setCurrentPage(index + 1);
         loadResponsesList(index + 1);
     }
 
-    const prepareTabledata = (data) => {
+    const prepareTableData = (data) => {
         let tableData = [];
         for (let i in data) {
             let item = data[i];
@@ -52,6 +54,49 @@ function ResultDetails() {
 
         }
         return tableData;
+    }
+
+    const prepareChartData = (response) => {
+        let chartData;
+        let labels = [];
+        let ids = [];
+        for (let i in question.choices) {
+            let choice = question.choices[i];
+            if (choice.choices) {
+                for (let j in choice.choices) {
+                    let subChoice = choice.choices[j];
+                    labels.push(choice.value + " - " + subChoice.value);
+                    ids.push(subChoice.id);
+                }
+            } else {
+                labels.push(choice.value);
+                ids.push(choice.id);
+            }
+        }
+        let data = [];
+        for (let i in ids) {
+            let id = ids[i];
+            let found = false;
+            for (let j in response) {
+                if (id === response[j].choiceId) {
+                    data.push(response[j].count);
+                    found = true;
+                    break;
+                }
+            }
+            if(found === false){
+                data.push(0);
+            }
+        }
+
+        chartData = {
+                "labels": labels,
+                "datasets": {
+                    "label": "Count",
+                    "data": data
+                }
+            }
+        return chartData;
     }
 
     const loadResponsesList = (pageNo) => {
@@ -69,7 +114,7 @@ function ResultDetails() {
             .then((response) => {
                 if (response.data.content.length !== 0) {
                     setTotalPage(response.data.totalPages);
-                    setAnswerData(prepareTabledata(response.data.content));
+                    setAnswerData(prepareTableData(response.data.content));
                 }
             })
             .catch((e) => {
@@ -81,29 +126,25 @@ function ResultDetails() {
     }
 
     const loadResponsesChart = (pageNo) => {
-        /*const token = localStorage.getItem('token');
+        const token = localStorage.getItem('token');
         axios
-            .get(url + "api/v1/analytics/responses", {
+            .get(url + "api/v1/analytics/responses-chart", {
                 headers: {
                     'Authorization': 'Bearer ' + token
                 },
                 params: {
-                    questionId: question.id,
-                    pageNo: pageNo
+                    questionId: question.id
                 }
             })
             .then((response) => {
-                if (response.data.content.length !== 0) {
-                    setTotalPage(response.data.totalPages);
-                    setAnswerData(prepareTabledata(response.data.content));
-                }
+                setChartData(prepareChartData(response.data));
             })
             .catch((e) => {
                 if (e.response.status === 403) {
                     localStorage.clear();
                     navigate('/authentication/sign-in')
                 }
-            })*/
+            })
     }
 
     function Descriptive() {
@@ -148,13 +189,38 @@ function ResultDetails() {
     }
 
     function NonDescriptive() {
+        return (
+            <MDBox>
+                {
+                    chartData ? <MDBox mt={4.5}>
+                        <Grid container spacing={1}>
+                            <Grid item lg={15}>
+                                <Grid container spacing={1} justifyContent="left" alignItems="center">
+                                    <Grid item xs={12} md={6} lg={10}>
+                                        <MDBox mb={3}>
+                                            <ReportsBarChart
+                                                color="info"
+                                                title={question.description}
+                                                chart={chartData}
+                                            />
+                                        </MDBox>
+                                    </Grid>
+                                    <Grid item>
 
+                                    </Grid>
+                                </Grid>
+                            </Grid>
+                        </Grid>
+                    </MDBox> : null
+                }
+            </MDBox>
+        )
     }
 
     useEffect(() => {
         if (question.questionType.name === "descriptive") {
             loadResponsesList(0);
-        }else{
+        } else {
             loadResponsesChart();
         }
     }, []);
