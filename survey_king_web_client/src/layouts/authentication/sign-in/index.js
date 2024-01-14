ba89e832384d7baa9cef5fcdba2c4f7c0c325b42
Abdullah Al Osman Sajid <abdullah.osman@tigerit.com>
2024-01-14 11:56:32 +0600

    Changes related to: 1.view changes

--- survey_king_web_client/src/layouts/authentication/sign-in/index.js
+++ survey_king_web_client/src/layouts/authentication/sign-in/index.js
@@ -89,7 +89,7 @@ function Basic() {
                             <MDTypography variant="h4" fontWeight="medium" color="white" mt={1}>
                                 Sign in
                             </MDTypography>
-                            <Grid container spacing={3} justifyContent="center" sx={{mt: 1, mb: 2}}>
+                            {/*<Grid container spacing={3} justifyContent="center" sx={{mt: 1, mb: 2}}>
                                 <Grid item xs={2}>
                                     <MDTypography component={MuiLink} href="#" variant="body1" color="white">
                                         <FacebookIcon color="inherit"/>
@@ -105,7 +105,7 @@ function Basic() {
                                         <GoogleIcon color="inherit"/>
                                     </MDTypography>
                                 </Grid>
-                            </Grid>
+                            </Grid>*/}
                         </MDBox>
                         <MDBox pt={4} pb={3} px={3}>
                             <MDBox component="form" role="form">
