7fb56605b0f57544105b584218b620ff993cb560
Abdullah Al Osman Sajid <abdullah.osman@tigerit.com>
2024-01-14 16:27:12 +0600

    Changes related to: 1.view changes

--- survey_king_web_client/src/pages/Presentation/index.js
+++ survey_king_web_client/src/pages/Presentation/index.js
@@ -69,7 +69,7 @@ function Presentation() {
             <MKTypography
               variant="h1"
               color="white"
-              mt={-20}
+              mt={-25}
               mb={1}
               sx={({ breakpoints, typography: { size } }) => ({
                 [breakpoints.down("md")]: {
@@ -84,7 +84,7 @@ function Presentation() {
               color="black"
               textAlign="center"
               px={{ xs: 6, lg: 12 }}
-              mt={-12}
+              mt={-17}
             >
               Free online survey software to collect and visualize data.
             </MKTypography>
