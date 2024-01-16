5d91088ca59985c75c5b114d50fac4832b76fe4d
Abdullah Al Osman Sajid <abdullah.osman@tigerit.com>
2024-01-16 12:36:12 +0600

    Changes related to: 1.view change

--- survey_king_web_client/src/layouts/authentication/components/BasicLayout/index.js
+++ survey_king_web_client/src/layouts/authentication/components/BasicLayout/index.js
@@ -71,7 +71,7 @@ function BasicLayout({image, errorMessage, children}) {
               SurveyDevs
           </MDTypography>*/}
             </MDBox>
-            <MDBox px={1} width="100%" mt={20} mx="auto">
+            <MDBox px={1} width="100%" height="100vh" mx="auto">
                 <Grid container spacing={1} justifyContent="center" alignItems="center" height="100%">
                     <Grid item xs={11} sm={9} md={5} lg={4} xl={3}>
                         {children}
